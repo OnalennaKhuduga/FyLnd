24c1b71fc43030638bced572a85df110ac0e075f
Onalenna Khuduga <khuduga6@gmail.com>
2026-08-05 15:41:08 +0200

    Configure React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
-)
+);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import { Heading } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return <Heading>FilmFind</Heading>;
+};
+
+export default HomePage;
