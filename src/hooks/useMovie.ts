407a00f6d20d8ec0342fecad466a1b557575a135
Onalenna Khuduga <khuduga6@gmail.com>
2026-08-20 19:29:57 +0200

    fix: add Movie type to useMovie hook

--- src/hooks/useMovie.ts
+++ src/hooks/useMovie.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import { Movie } from "./useMovies";
 
 const useMovie = (id: number) => {
-  const apiClient = new APIClient("/movie");
+  const apiClient = new APIClient<Movie>("/movie");
 
   return useQuery({
     queryKey: ["movie", id],
