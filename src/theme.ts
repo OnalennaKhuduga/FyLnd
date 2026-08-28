f41838874cac1f01594cb64508ad3a0f06fe5639
Onalenna Khuduga <khuduga6@gmail.com>
2026-08-28 14:02:41 +0200

    fix: set dark mode as default theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
 const theme = extendTheme({
+  config,
   colors: {
     gray: {
       50: "#eef0f6",
